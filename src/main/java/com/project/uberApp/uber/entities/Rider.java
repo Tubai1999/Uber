b809bdbc569fb1a1ee7c061c8d99264f24154668
Tridib Samanta <tridibsamanta311@gmail.com>
2024-12-04 23:20:31 +0530

    initial commit

---
+++ src/main/java/com/project/uberApp/uber/entities/Rider.java
@@ -0,0 +1,16 @@
+package com.project.uberApp.uber.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Rider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private Double rating;
+
+}
---
+++ src/test/java/com/project/uberApp/uber/UberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.uberApp.uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
